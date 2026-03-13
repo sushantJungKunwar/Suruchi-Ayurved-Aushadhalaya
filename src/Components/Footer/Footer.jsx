576e1720800f0dbba92d9307575dffad3c436697
Sushant Jung Kunwar <sushantkch@gmail.com>
2026-03-13 16:31:37 +0545

    try

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>©2026 Suruchi Aayurvedic. All right reserved.</p>
+      <ul>
+        <li>Terms of service</li>
+        <li>Privacy Policy</li>
+        <li>Contact us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
